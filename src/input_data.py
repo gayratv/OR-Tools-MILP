bbc6c922be948b24c5067c20a87347c1e7498c96
Gayrat Vlasov <ggvlasov@gmail.com>
2025-09-09 21:53:29 +0300

    сработал со всеми ограничениями

--- src/input_data.py
+++ src/input_data.py
@@ -225,8 +225,8 @@ class OptimizationWeights:
 
 @dataclass
 class OptimizationGoals:
-    teacher_slot_optimization: bool = False
+    teacher_slot_optimization: bool = True
 
     # Предметы, запрещённые последними уроками по параллелям
-    subjects_not_last_lesson_optimization: bool = False
-    print_timetable_to_console: bool = True
+    subjects_not_last_lesson_optimization: bool = True
+    print_timetable_to_console: bool = False
