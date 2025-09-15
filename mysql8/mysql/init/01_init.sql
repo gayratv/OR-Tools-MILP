43cf3b52476637fd6b58f3deeb29326a6ae97f8d
Gayrat Vlasov <ggvlasov@gmail.com>
2025-09-15 11:00:48 +0300

    mysql docker

--- mysql8/mysql/init/01_init.sql
+++ mysql8/mysql/init/01_init.sql
@@ -1,8 +1,18 @@
 -- Выполняется один раз при первом старте, если данных ещё нет
 -- Создаст схему/права, более строгие гранты и host-ограничение (по IP при необходимости)
 -- Пример первичной инициализации
-CREATE DATABASE IF NOT EXISTS `${MYSQL_DATABASE}` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 
--- Пример явной выдачи прав конкретному пользователю (дублирует то, что делает entrypoint, но гибче)
-GRANT ALL PRIVILEGES ON `${MYSQL_DATABASE}`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+      -- Создание базы (если её ещё нет)
+CREATE DATABASE IF NOT EXISTS `${MYSQL_DATABASE}`
+  /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+-- Основной пользователь (полные права на БД)
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON `${MYSQL_DATABASE}`.* TO '${MYSQL_USER}'@'%';
+
+-- Обычный пользователь (только CRUD-операции)
+CREATE USER IF NOT EXISTS '${MYSQL_RW_USER}'@'%' IDENTIFIED BY '${MYSQL_RW_PASSWORD}';
+GRANT SELECT, INSERT, UPDATE, DELETE
+  ON `${MYSQL_DATABASE}`.* TO '${MYSQL_RW_USER}'@'%';
+
 FLUSH PRIVILEGES;
