af8e3312369634f1636aeb02581d4b894c0964df
Gayrat Vlasov <ggvlasov@gmail.com>
2025-09-18 13:07:01 +0300

    работающий cloud-init.yaml

--- ya-cloud/create-docker.sh
+++ ya-cloud/create-docker.sh
@@ -49,6 +49,8 @@ need jq
 echo "Создание ВМ: name=${VM_NAME}, platform=${PLATFORM}, cores=${CORES}, mem=${MEMORY}GB, disk=${DISK_SIZE}GB" >&2
 
 # -------- Создание ВМ ------------------------------------------------
+# Передаем user-data и подставляем в него переменную ssh_key из файла --metadata-from-file
+
 RESP=$(
   yc compute instance create \
     --name "$VM_NAME" \
@@ -60,7 +62,6 @@ RESP=$(
     --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2204-lts,size="$DISK_SIZE" \
     --public-ip \
     --service-account-name sc-scheduller-srv-acc \
-    --metadata "ssh-keys=$(cat ~/.ssh/ya-cloud/priv.pub)" \
     --metadata-from-file user-data=./ya-cloud/cloud-init.yaml \
     --format json
 )
@@ -80,3 +81,23 @@ jq -r '.id as $id
        + "\n  External:  " + (first(.network_interfaces[].primary_v4_address.one_to_one_nat.address // empty) // "")' <<< "$RESP"
 echo "----------------------------------------"
 echo "Экспортировано: VM_EXTERNAL_IP=${VM_EXTERNAL_IP}"
+
+echo "Ожидание доступности SSH на ${VM_EXTERNAL_IP}:22..."
+
+#ATTEMPTS=0
+#MAX_ATTEMPTS=30 # ~1 минута
+#
+#while ! nc -z -w 2 "$VM_EXTERNAL_IP" 22 && [[ $ATTEMPTS -lt $MAX_ATTEMPTS ]]; do
+#  sleep 2
+#  ATTEMPTS=$((ATTEMPTS + 1))
+#  printf "."
+#done
+#
+#echo "" # Новая строка после точек
+#
+#if [[ $ATTEMPTS -eq $MAX_ATTEMPTS ]]; then
+#  echo "Ошибка: не удалось дождаться запуска SSH-сервера на ВМ." >&2
+#  exit 1
+#fi
+#
+#echo "SSH-сервер готов. Можно подключаться."
