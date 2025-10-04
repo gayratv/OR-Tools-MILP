ab445f21977bdea93b09a0ed3ffb9dba9619e8ee
Gayrat Vlasov <ggvlasov@gmail.com>
2025-10-04 13:00:49 +0300

    разнес файлы по поддиректориям

--- docker-python3.12/python-app/schedule_calculator.py
+++ docker-python3.12/python-app/schedule_calculator.py
@@ -37,9 +37,9 @@ from data_types.input_data import InputData, OptimizationGoals
 from input_data.optimization_weights import OptimizationWeights, get_default_optimization_weights
 from input_data.rasp_data import create_manual_data
 from access_loader import load_data_from_access
-from rasp_data_generated import create_timetable_data
+from milp_equations.rasp_data_generated import create_timetable_data
 from utils.print_schedule import get_solution_maps, export_full_schedule_to_excel, print_schedule_to_console
-from teacher_windows_opus import add_teacher_window_optimization_span
+from milp_equations.teacher_windows_opus import add_teacher_window_optimization_span
 from utils.put_to_bucket import upload_file_to_s3
 from mysql_io.save_results_to_mysql import save_calculation_results
 from utils.helpers import validate_input_data, calculate_teacher_windows
