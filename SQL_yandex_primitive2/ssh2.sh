4a839ef099684a90a8a36151143dae9b5bbd4313
Gayrat Vlasov <ggvlasov@gmail.com>
2025-10-14 01:06:17 +0300

    db backup

--- SQL_yandex_primitive2/ssh2.sh
+++ SQL_yandex_primitive2/ssh2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Подключаюсь к $VM_EXTERNAL_IP..."
 echo "Время выполнения скрипта: $SECONDS сек."
-export VM_EXTERNAL_IP=
-ssh -i ~/.ssh/ya-cloud/priv yc-user@
+export VM_EXTERNAL_IP=51.250.86.191
+ssh -i ~/.ssh/ya-cloud/priv yc-user@51.250.86.191
